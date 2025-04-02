488e61d5464d7c28ba7b02b223120cfc269b6b98
Martensven <martengullstrom@gmail.com>
2025-04-02 09:15:39 +0200

    updated user-route and server.js

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import { User } from '../models/users.js';
 
+// Middleware
+const app = express();
+app.use(express.json());
+
 const router = express.Router();
 
 // Hämta användare
@@ -30,6 +34,11 @@ router.post('/register', async (req, res) => {
             return res.status(400).json({ error: 'All fields are required' });
         }
 
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: 'Failed to create user, E-mail already exists' });
+        }
+
         const newUser = new User(req.body);
         await newUser.save();
         res.status(201).json({ message: 'User created successfully', user: newUser });
@@ -44,7 +53,6 @@ router.put('/:id', async (req, res) => {
         const { id } = req.params;
         const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
 
-<
         if (!updatedUser) {
             return res.status(404).json({ message: 'User not found' });
         }
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 // Imports
 import express from 'express';
 import { connectDB } from './config/db.js';
+import { middleWare } from './middlewares/middleware.js';
 import userRoutes from './routes/user-route.js';
 import itemRoutes from './routes/item-route.js';
 import orderRoutes from './routes/order-route.js'
 import cartRoutes from './routes/cart-route.js';
 
-import { middleWare } from './middlewares/middleware.js';
-
 const app = express(); // Skapa app först!
 const PORT = 4321;
 
 // Middleware
+app.use(express.json()); // Lägg till denna rad
 await middleWare();
 
 // Routes
@@ -19,6 +19,8 @@ app.use('/user', userRoutes);
 app.use('/items', itemRoutes);
 app.use('/orders', orderRoutes);
 app.use('/cart', cartRoutes);
+
+
 
 // Anslut till databasen och starta servern
 connectDB()
@@ -35,6 +37,7 @@ connectDB()
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
+
 
 
 
