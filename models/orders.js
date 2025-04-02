f755c1912fd02257daa73a39025ca5f96764e47c
Mussse97 <89797827+Mussse97@users.noreply.github.com>
2025-04-02 08:50:07 +0200

    readme

--- models/orders.js
+++ models/orders.js
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 const orderSchema = new mongoose.Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     total_price: { type: Number, required: true },
