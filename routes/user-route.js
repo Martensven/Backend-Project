f62edbc3640b132ead2bd92e10f23eb40dbf81d4
Emil Asikainen <emilasikainen@hotmail.com>
2025-04-02 16:07:56 +0200

    Uppdaterat koden med nya ändringar från github samt den kod som jag har nu som får kundvagnen att fungera

--- routes/user-route.js
+++ routes/user-route.js
@@ -44,7 +44,6 @@ router.put('/:id', async (req, res) => {
         const { id } = req.params;
         const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
 
-<
         if (!updatedUser) {
             return res.status(404).json({ message: 'User not found' });
         }
