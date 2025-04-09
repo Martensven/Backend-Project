c380a4230289e225a0cdfaee0ccb7afa2290adc8
Emil Asikainen <emilasikainen@hotmail.com>
2025-04-09 12:25:40 +0200

    Update middleware.js updated for cart-route authorization to work

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -2,14 +2,21 @@ import express from 'express';
 import jwt from 'jsonwebtoken';
 
 export const middleWare = (app) => {
-    app.use(express.json()); //bara express.json() middleware
+    app.use(express.json()); // bara express.json() middleware
 };
+
 // Middleware för att verifiera JWT-token
 export const authMiddleware = (req, res, next) => {
     const token = req.header('Authorization');
-    if (!token) return res.status(401).json({ error: 'Access denied. No token provided.' });
+
+    if (!token) {
+        // Ingen token innebär att vi tillåter gästanvändare
+        req.user = null;
+        return next(); // Fortsätt till nästa middleware
+    }
 
     try {
+        // Verifiera token om den finns
         const decoded = jwt.verify(token.replace('Bearer ', ''), process.env.JWT_SECRET);
         req.user = decoded;
         next();
