afb4c5edee7a206c18a01c3f039cb42de133fedf
Emil Asikainen <emilasikainen@hotmail.com>
2025-04-09 12:15:44 +0200

    Update server.js With the new cart-route code to make user/guest work

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Imports
 import express from 'express';
+import session from 'express-session';
+import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import { middleWare } from './middlewares/middleware.js';
 import userRoutes from './routes/user-route.js';
@@ -7,20 +9,18 @@ import itemRoutes from './routes/item-route.js';
 import orderRoutes from './routes/order-route.js';
 import cartRoutes from './routes/cart-route.js';
 import aboutRoutes from './routes/about-route.js';
+import cors from 'cors';
 
-import cookieParser from 'cookie-parser';
-import session from 'express-session';
-
-import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express(); // Skapa app först!
 const PORT = 4321;
 
 // Middleware
-app.use(middleWare());
+middleWare(app); // Använd middleware för att parsa JSON-data
+// app.use(middleWare());
+app.use(cors());
 app.use(express.json());
-app.use(cookieParser());
 app.use(session({
     secret: 'sample-secret',
     resave: false,
